de7f8cf00057c25cf9f0002beaa3cefda77c5f8d
jonathan_baraza <jbaraza46@gmail.com>
2023-11-18 11:23:49 +0300

    toastify alerts init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Providers } from '@/redux/Providers'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Providers } from "@/redux/Providers";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Budget tracker app',
-  description: 'An application for tracking personal expenses and income to manage a budget.',
-}
+  title: "Budget tracker app",
+  description:
+    "An application for tracking personal expenses and income to manage a budget.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <ToastContainer />
+        </Providers>
       </body>
     </html>
   );
